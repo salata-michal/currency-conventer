b64af3db7f4127ebdc24f8ed9fe64bea003dfd9b
Michal Salata <salata.michal1@gmail.com>
2022-12-29 20:29:39 +0100

    background change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 {
-
     const calculateResult = (amount, currency) => {
         const rateEUR = 4.6886;
         const rateGBP = 5.3926;
         const rateUSD = 4.4153;
         const rateCHF = 4.7453;
-
 
         switch (currency) {
             case "EUR":
@@ -23,6 +21,7 @@
     };
 
     const resultText = (amount, currency, result) => {
+        
         const resultElement = document.querySelector(".js-result");
         resultElement.innerText = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
     };
